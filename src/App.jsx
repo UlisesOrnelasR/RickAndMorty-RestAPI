b04d7bdf9fd47b04ae4772385d5265771bf47a8b
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-07-04 16:40:48 -0600

    Haciendo fetch al API rick and morty

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import { useEffect } from "react";
+
 export const App = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://rickandmortyapi.com/api/character");
+      const data = await response.json();
+      console.log(data);
+    };
+    fetchData();
+  }, []);
+
   return <div>App</div>;
 };
