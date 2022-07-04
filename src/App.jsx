c4597efd329787ce990cbd5193cf73a76e0e199e
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-07-04 17:15:55 -0600

    Haciendo lista con los elementos obtenidos del fetch

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,19 @@ export const App = () => {
     const fetchData = async () => {
       const response = await fetch("https://rickandmortyapi.com/api/character");
       const { results } = await response.json();
-      console.log(results);
+      setCharacters(results);
     };
     fetchData();
   }, []);
 
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>Rick and Morty</h1>
+      <ol>
+        {characters.map((charachter) => {
+          return <li>{charachter.name}</li>;
+        })}
+      </ol>
+    </div>
+  );
 };
