d54dcf95a1b8b9481bb2088c4d1f682431a7c1cd
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-07-04 21:36:34 -0600

    Añadiendo estilo con bootstrap, creando estado para el loading de datos

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -2,21 +2,29 @@ import { useEffect, useState } from "react";
 import { Character } from "./Character";
 export const CharacterList = () => {
   const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch("https://rickandmortyapi.com/api/character");
       const { results } = await response.json();
       setCharacters(results);
+      setLoading(false);
     };
     fetchData();
   }, []);
 
   return (
-    <div>
-      {characters.map((character) => {
-        return <Character key={character.id} character={character} />;
-      })}
+    <div className="container bg-danger">
+      <div className="row">
+        {characters.map((character) => {
+          return (
+            <div className="col-md-4" key={character.id}>
+              <Character character={character} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
