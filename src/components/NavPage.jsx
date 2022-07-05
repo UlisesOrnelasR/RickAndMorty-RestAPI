7562f98022fc37ae0d15294f5aa92d739374bd30
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-07-04 22:25:49 -0600

    Corrigiendo numero de pagina

--- src/components/NavPage.jsx
+++ src/components/NavPage.jsx
@@ -3,7 +3,7 @@ export const NavPage = ({ setPage, page }) => {
     <div className="d-flex justify-content-between align-items-center">
       <p>Page: {page}</p>
       <button className="btn btn-info btn-sm" onClick={() => setPage(page + 1)}>
-        Page: {page}
+        Page: {page + 1}
       </button>
     </div>
   );
