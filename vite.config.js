aef77b2b7fcd9dec0d7c6a1b54241635bfb2e587
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-07-04 22:34:05 -0600

    base url del proyecto

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://UlisesOrnelasR.github.io/RickAndMorty-RestAPI/',
   plugins: [react()]
 })
