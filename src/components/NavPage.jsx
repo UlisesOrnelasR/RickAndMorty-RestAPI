cfa31498cfab10503832c3bd679f4b51ee755457
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-07-04 22:59:46 -0600

    Haciendo que el usuario no pueda ir a pagina < 1

--- src/components/NavPage.jsx
+++ src/components/NavPage.jsx
@@ -1,6 +1,12 @@
 export const NavPage = ({ setPage, page }) => {
   return (
     <div className="d-flex justify-content-between align-items-center">
+      <button
+        className="btn btn-info btn-sm"
+        onClick={() => (page <= 1 ? alert("No hay data") : setPage(page - 1))}
+      >
+        Page: {page - 1}
+      </button>
       <p>Page: {page}</p>
       <button className="btn btn-info btn-sm" onClick={() => setPage(page + 1)}>
         Page: {page + 1}
